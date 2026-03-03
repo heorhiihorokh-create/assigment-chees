9fb836f5607fb1ba40a3a452d341299b8a7b47a6
Heorhii Horokh <Heorhii.HOROKH@student.howest.be>
2026-03-03 02:19:01 +0100

    chore: demonstrate basic move in main

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from board import Board
 def main() -> None:
     board = Board()
     board.setup_board()
+
+    print("Initial position:")
     board.print_board()
 
-if __name__ == "__main__":
-    main()
+    print("\nMove: e2 -> e4")
+    board.move_piece("e2", "e4")
+    board.print_board()
